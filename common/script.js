b1833c94ff8d27b4835e389611e1c837821645c8
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-09-10 23:59:02 +0700

    first commit

---
+++ common/script.js
@@ -0,0 +1,11 @@
+document.getElementById('register-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Thực hiện xử lý đăng ký ở đây
+    alert('Đăng ký thành công');
+});
+
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Thực hiện xử lý đăng nhập ở đây
+    alert('Đăng nhập thành công');
+});
