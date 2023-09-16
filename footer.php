ebeddb632da2cf36e95122e4bece3adfdb962174
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-09-16 13:47:16 +0700

    16/9

---
+++ footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./boottrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./fontawesome-free-6.3.0-web/css/all.css">
+</head>
+
+<body>
+    <footer class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <h1>DBHD</h1>
+                <p>mang trong mình khát khao định hình lại thị trường nhà trọ truyền thống thông qua việc nâng cao tiêu
+                    chuẩn các căn hộ và ứng dụng chuyển đổi số. Từ đó xây dựng hệ sinh thái căn hộ cho thuê trải dài
+                    trên toàn lãnh thổ Việt Nam, mang lại môi trường sống hạnh phúc, tiện nghi cho cộng đồng những người
+                    đi thuê nhà.</p>
+                <div><i class="fa-brands fa-facebook" style="color: #ffc05f;"></i>   <i class="fa-brands fa-youtube"
+                        style="color: #ffc05f;"></i></div>
+            </div>
+            <div class="col-md-3">
+                <h1>Về chúng tôi</h1>
+                <p><a>Về chúng tôi</a></p>
+                <p><a>Toà nhà</a></p>
+                <p><a>Tin tức</a></p>
+            </div>
+            <div class="col-md-3">
+                <h1>Chính sách</h1>
+                <p><a>Hợp tác</a></p>
+                <p><a>Liên hệ</a></p>
+            </div>
+            <div class="col-md-3">
+                <h1>Liên hệ</h1>
+                <p><i class="fa-solid fa-envelope" style="color: #ffc05f;"></i>  <a>nhom9dhh11c12@gmail.com</a></p>
+                <p><i class="fa-solid fa-mobile-screen" style="color: #ffc05f;"></i>  <a>0368966992</a></p>
+                <p><i class="fa-solid fa-location-dot" style="color: #ffc05f;"></i>  41A Đ. Phú Diễn, Phú Diễn, Bắc Từ Liêm, Hà Nội, Việt Nam</p>
+            </div>
+        </div>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.655069674606!2d105.76220289999999!3d21.0464832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454c3ce577141%3A0xb1a1ac92701777bc!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBUw6BpIG5ndXnDqm4gdsOgIE3DtGkgdHLGsOG7nW5nIEjDoCBO4buZaQ!5e0!3m2!1svi!2s!4v1694837264947!5m2!1svi!2s"
+            width="100%" height="200" style="border:0; padding: 0px;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- trangchu.php
+++ trangchu.php
@@ -6,14 +6,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-       <?php 
-       if (isset($_SESSION['username']) && $_SESSION['username']){
-           echo 'Bạn đã đăng nhập với tên là '.$_SESSION['username']."<br/>";
-           echo 'Click vào đây để <a href="logout.php">Logout</a>';
-       }
-       else{
-           echo 'Bạn chưa đăng nhập';
-       }
-       ?>
+       <?php include('header.php'); ?>
+        <?php include('trangchu.html');?>
+       <?php include('footer.php');?>
     </body>
 </html>
